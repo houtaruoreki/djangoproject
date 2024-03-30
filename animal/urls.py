9910bcca6d4bf8041343f86a990f669226d19e5d
houtaruoreki <l.maghlaperidze@gmail.com>
2024-03-30 13:21:01 +0400

    AnimalView url added

--- animal/urls.py
+++ animal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AnimalListView.as_view(), name='AllAnimals'),
+    path('<int:pk>/', views.AnimalView.as_view(), name='GetAnimal'),
     path('add', views.AddAnimalView.as_view(), name='AddAnimal'),
     path('update/<int:pk>', views.UpdateAnimalView.as_view(), name='UpdateAnimal'),
     path('delete/<int:pk>', views.DeleteAnimalView.as_view(), name='DeleteAnimal')
